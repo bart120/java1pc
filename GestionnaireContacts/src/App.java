0ac91047f87b6ec8f56f5ffcadb3778b7e10ae84
Vincent Leclerc <vleclerc@inow.fr>
2023-01-09 14:43:07 +0100

    menu

--- GestionnaireContacts/src/App.java
+++ GestionnaireContacts/src/App.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+
+    private static Scanner _scan = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        while (true) {
+            afficherMenu();
+            String choix = _scan.nextLine();
+            switch (choix) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "q":
+                    return;
+                default:
+                    System.out.println("Boulet !!!");
+                    break;
+            }
+        }
+    }
+
+    private static void afficherMenu() {
+        ArrayList<String> menus = new ArrayList<>();
+        menus.add("-- MENU --");
+        menus.add("1- Ajouter un contact");
+        menus.add("2- Lister les contacts");
+        menus.add("q- Quitter");
+        for (String menu : menus) {
+            System.out.println(menu);
+        }
     }
 }
